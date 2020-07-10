80e8c0f15a8a81c60bafce649bed88c2535e1b24
Jonas <jonasbuchholz@freenet.de>
2020-07-10 11:52:08 +0200

    Improves variable naming

--- my_source.py
+++ my_source.py
@@ -1,11 +1,11 @@
 """Contains code under test for solution_ci."""
 
-def add_func(a, var_two):
+def add_func(var_one, var_two):
     """
     Add two variables.
 
     Args
-        a: first variable
+        var_one: first variable
         var_two: second variable
 
     """
